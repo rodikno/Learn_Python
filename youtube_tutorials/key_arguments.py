8f3393a1736d2513cb3df8430259900ee8db4940
Rodion Baronov <rodioba@247e.com>
2017-06-11 23:21:28 +0300

    Key (named) arguments, scopes

---
+++ youtube_tutorials/key_arguments.py
@@ -0,0 +1,6 @@
+def dumb_sentence(name='Vasyl', action='added', item='image'):
+    print(name, action, item)
+
+dumb_sentence()
+dumb_sentence('Nina', 'makes', 'lunch')
+dumb_sentence(item='friend', action='is')
---
+++ youtube_tutorials/visibility_scopes.py
@@ -0,0 +1,11 @@
+a = 6589
+
+def corn():
+    n = 2569
+    print(n)
+
+def fudge():
+    print(a)
+
+corn()
+fudge()
