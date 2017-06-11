5106c9f3967835c73f9f36224cdbf20201bcba0d
Rodion Baronov <rodioba@247e.com>
2017-06-11 23:10:23 +0300

    functions, return operator, default parameter values

---
+++ youtube_tutorials/functions.py
@@ -0,0 +1,9 @@
+def beef():
+    print("Beef is ready")
+
+def bitcoin_to_usd(btc):
+    amount = btc * 527
+    print(amount)
+
+beef()
+bitcoin_to_usd(15)
---
+++ youtube_tutorials/operator_return.py
@@ -0,0 +1,6 @@
+def allowed_dating_age(my_age):
+    girls_age = my_age//2 + 7
+    return girls_age
+
+new = allowed_dating_age(24)
+print(new)
